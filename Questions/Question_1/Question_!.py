d3c2e5384d3b51736848a6d75a98c6dad68a19d1
AnalyticsForPleasure <shaycohen2003@gmail.com>
2024-04-07 07:42:33 +0300

    Uploading the data + addin finding the unique values in each column.

--- Questions/Question_1/Question_!.py
+++ Questions/Question_1/Question_!.py
@@ -4,6 +4,14 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 
+# Scenario
+# A fast-food chain plans to add a new item to its menu. However, they are still undecided between three possible marketing campaigns for promoting the new product.
+# In order to determine which promotion has the greatest effect on sales, the new item is introduced at locations in several randomly selected markets. A different
+# promotion is used at each location, and the weekly sales of the new item are recorded for the first four weeks.
+#
+# Goal
+# Evaluate A/B testing results and decide which marketing strategy works the best.
+
 
 
 # MarketID: unique identifier for market
@@ -23,3 +31,10 @@ if __name__ == '__main__':
     print('*')
     column_headers = list(df.columns.values)
     print("The Column Header :", column_headers)
+
+    MarketSize_values = pd.unique(df['MarketSize'])# 'Medium', 'Small'.'Large'
+    LocationID_values = pd.unique(df['LocationID'])
+    Age_of_Store_values = pd.unique(df['AgeOfStore']) # range of years : 1 - 23 years.
+    Promotion_values = pd.unique(df['Promotion']) # [3,2,1]
+    Week_values = pd.unique(df['week']) # [1,2,3,4]
+    print('*')
